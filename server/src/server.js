ab84b0e91597a6ee2a45dc617a7281d9ba4d33e2
roman.datsyuk <r.datsyuk@levi9.com>
2018-12-03 10:32:31 +0200

    Fix server.js comments

--- server/src/server.js
+++ server/src/server.js
@@ -8,12 +8,10 @@ dotenv.config();
 const app = express();
 const PORT = 3000;
 
-// Middleware for that parsing incoming requests with JSON payloads
+// Middleware for parsing incoming requests with JSON payloads
 app.use(express.json());
 
-// Root API that says that app is working
 app.get('/', (req, res) => res.send('App works!'));
-
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 
