ecad533bcfac4c10f105152a27fc9578375a9743
bingex <romandatsyuk@gmail.com>
2018-12-04 18:43:54 +0200

    Token initialization on app start. historyApiFallback: true for page refresh

--- interface/src/components/App.js
+++ interface/src/components/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import jwtDecode from 'jwt-decode';
+import { useDispatch } from 'redux-react-hook';
 import AppRouter from './../routes';
+import setAuthToken from './../utils/setAuthToken';
+import { setCurrentUser } from './../store/actions/authActions';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  if (localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+    dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
+  }
+
   return <AppRouter />;
 }
--- interface/webpack.config.js
+++ interface/webpack.config.js
@@ -12,6 +12,9 @@ module.exports = {
     path: path.resolve('dist'),
     filename: 'bundle.js'
   },
+  devServer: {
+    historyApiFallback: true
+  },
   module: {
     rules: [
       {
